d2a30e0accce16ab53a4c2a9ccd159b5acb8bbcf
drodedavi <73032991+drodedavi@users.noreply.github.com>
2020-10-17 18:38:04 +0200

    Update like.py

--- userbot/plugins/like.py
+++ userbot/plugins/like.py
@@ -25,9 +25,9 @@ from userbot.system import dev_cmd
 
 @bot.on(dev_cmd("like", outgoing=True))
 async def like(event):
-    await event.edit("**👍🏻 a** [Marteⁿᵃᶻ💎🔥 ⁪⁬⁮⁮⁮⁮](t.me/dubitante) **piace questo elemento.**")
+    await event.edit("**👍🏻 a** [Dανiδe🐰🥕⁪⁬⁮⁮⁮⁮](t.me/notdavi) **piace questo elemento.**")
     
 
 @bot.on(dev_cmd("dislike", outgoing=True))
 async def dislike(event):
-    await event.edit("**👎🏻 a** [Marteⁿᵃᶻ💎🔥 ⁪⁬⁮⁮⁮⁮](t.me/dubitante) **non piace questo elemento.**")
+    await event.edit("**👎🏻 a** [Dανiδe🐰🥕⁪⁬⁮⁮⁮⁮](t.me/notdavi) **non piace questo elemento.**")
